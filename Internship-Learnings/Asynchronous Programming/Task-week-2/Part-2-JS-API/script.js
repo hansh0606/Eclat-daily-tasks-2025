2fb5b351c64df0713263418651a8e44a79aec93d
“hansh0606” <hanshpatil1982@gmail.com>
2025-01-26 23:28:37 +0530

    [Updated] : Added one function in curriculum task

--- Internship-Learnings/Asynchronous Programming/Task-week-2/Part-2-JS-API/script.js
+++ Internship-Learnings/Asynchronous Programming/Task-week-2/Part-2-JS-API/script.js
@@ -57,7 +57,7 @@ const fetch_data = async()=>{
     console.log(`✵Body: ${longestTitlePost.body}`);
     console.log("✵Length of Title : "+ max_title)
 
-    //Number of post for specific user
+    //Total Number of posts
     const post_count = json.reduce((acc,curr)=>{
         if(acc.userId==curr.userId)
         {
@@ -66,14 +66,52 @@ const fetch_data = async()=>{
         return acc+1
     },0)
     console.log("\n⭐Number of posts:" + post_count)
+    console.log("\n⭐Posts per User:")
+    //count Number of posts for each User
+    const postCountsByUserId = json.reduce((acc, curr) => {
+        if(acc[curr.userId])
+        {
+            acc[curr.userId] = acc[curr.userId]+1
+        }
 
-    // const count_eachPost = json.reduce((acc,curr)=>{
-    //     if(!acc[curr.userId])
-    //     return acc
-    // },{})
-   
+        else{
+            acc[curr.userId] = 1
+        }
+
+        return acc;
+    }, {});
+
+    for(let userId in postCountsByUserId ){
+        console.log("UserId "+userId+":"+postCountsByUserId[userId]+" posts")
+
+    } 
 }
 
 fetch_data()
 
-  
+ //Output:
+ /*
+ API is in process...
+
+⭐Post with the longest title:
+✵User ID: 5
+✵Title: repellendus qui recusandae incidunt voluptates tenetur qui omnis exercitationem
+✵Body: error suscipit maxime adipisci consequuntur recusandae
+voluptas eligendi et est et voluptates
+quia distinctio ab amet quaerat molestiae et vitae
+adipisci impedit sequi nesciunt quis consectetur
+✵Length of Title : 79
+
+⭐Number of posts:100
+⭐Posts per User:
+UserId 1:10 posts
+UserId 2:10 posts
+UserId 3:10 posts
+UserId 4:10 posts
+UserId 5:10 posts
+UserId 6:10 posts
+UserId 7:10 posts
+UserId 8:10 posts
+UserId 9:10 posts
+UserId 10:10 posts
+*/ 
