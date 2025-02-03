634c0bd2ee00b294c8acee7a3cef0e42c0a035b8
“hansh0606” <hanshpatil1982@gmail.com>
2025-02-03 16:59:34 +0530

    Profile Card Basic structure with Basic styling

--- Internship-Learnings/ReactNative/BasicProject/App.js
+++ Internship-Learnings/ReactNative/BasicProject/App.js
@@ -1,97 +1,89 @@
 import React from 'react';
-import {FlatList, Image, ScrollView, Text, View} from 'react-native';
+import {Image, Pressable, StyleSheet, Text, View, FlatList} from 'react-native';
+//import { FlatList } from "react-native-web";
 
-const demoList = [
-  {id: 1, name: 'Hanshvee'},
-  {id: 2, name: 'Hriday'},
-  {id: 3, name: 'Heet'},
+const projects = [
+  {
+    id: '1',
+    projectName: 'InQgen',
+    description: 'A platform that generates interview questions from resumes.',
+    technologies: 'React.js, PyPDF2, OpenAI',
+  },
+  {
+    id: '2',
+    projectName: 'Landmine Detection',
+    description:
+      'AI/ML-based object detection system for landmine categorization.',
+    technologies: 'YOLO, AI, Object Detection',
+  },
+  {
+    id: '3',
+    projectName: 'Sports Central',
+    description:
+      'A Spring Boot Java application with CRUD functionality for efficient sports event management.',
+    technologies: 'Spring Boot, Hibernate, React, MySQL',
+  },
+  {
+    id: '4',
+    projectName: 'KabaddiStrive',
+    description:
+      'A PostgreSQL-based console application for Kabaddi league management, handling courts, teams, matches, and players.',
+    technologies: 'PostgreSQL',
+  },
+  {
+    id: '5',
+    projectName: 'Boxico',
+    description:
+      'A platform connecting boxers with trainers for personalized training experiences.',
+    technologies: 'React.js, Node.js, MongoDB',
+  },
 ];
-const CustomComponent = () => {
-  return (
-    <View>
-      <Text>Custom Component</Text>
-    </View>
-  );
-};
-const getFullName = (Fname, MName, SName) => {
-  return Fname + ' ' + MName + ' ' + SName;
-};
 
 function App() {
   return (
-    <ScrollView>
-      <View>
-        <Text>
-          Text Component : {getFullName('Hanshvee', 'Aditya', 'Patil')}
-        </Text>
-        <Text>{getFullName('Hriday', 'Aditya', 'Patil')}</Text>
-        <Text style={{padding: 20}}>
-          About Me: I'm Hanshvee Patil IT student from Dharmsinh Desai
-          University. With a deep passion for coding and design, I am a tech
-          enthusiast eager to make my mark in the digital realm. Beyond IT, my
-          talents extend to being a national-level boxer and an adventurous
-          mountaineer and trekker. My creative mindset merges with analytical
-          prowess to craft captivating and user-friendly interfaces, with the
-          goal of revolutionizing user experiences. Through my sportsmanship and
-          honed leadership skills, I aspire to leave a lasting impact on the IT
-          landscape, shaping a future where technology empowers and enhances
-          lives. Keep an eye on me, an IT prodigy and intrepid adventurer,
-          destined to achieve remarkable feats and inspire those around me.
-        </Text>
+    <>
+      <View style={{display: 'flex', alignItems: 'center'}}>
+        <Text style={{fontSize: 30, fontWeight: 'bold'}}>Hanshvee Patil</Text>
+      </View>
+      <View style={{display: 'flex', alignItems: 'center'}}>
         <Image
-          source={require('./src/assets/BabyHanshvee.jpg')}
-          style={{width: 200, height: 200}}></Image>
-        <FlatList
-          data={demoList}
-          renderItem={({item}) => (
-            <Text>
-              {item.id} {item.name}
-            </Text>
-          )}
-          keyExtractor={item => item.id.toString()}
+          source={require('./src/assets/HanshveeFormalPhoto.jpg')}
           style={{
-            marginTop: 20,
-            backgroundColor: '#1BFC06',
-          }}></FlatList>
-        <CustomComponent />
+            width: 100,
+            height: 100,
+            borderRadius: 100,
+          }}></Image>
+        <Text>hanshpatil1982@gmail.com</Text>
       </View>
 
-      <View>
-        <Text>
-          Text Component : {getFullName('Hanshvee', 'Aditya', 'Patil')}
-        </Text>
-        <Text>{getFullName('Hriday', 'Aditya', 'Patil')}</Text>
-        <Text style={{padding: 20}}>
-          About Me: I'm Hanshvee Patil IT student from Dharmsinh Desai
-          University. With a deep passion for coding and design, I am a tech
-          enthusiast eager to make my mark in the digital realm. Beyond IT, my
-          talents extend to being a national-level boxer and an adventurous
-          mountaineer and trekker. My creative mindset merges with analytical
-          prowess to craft captivating and user-friendly interfaces, with the
-          goal of revolutionizing user experiences. Through my sportsmanship and
-          honed leadership skills, I aspire to leave a lasting impact on the IT
-          landscape, shaping a future where technology empowers and enhances
-          lives. Keep an eye on me, an IT prodigy and intrepid adventurer,
-          destined to achieve remarkable feats and inspire those around me.
-        </Text>
-        <Image
-          source={require('./src/assets/BabyHanshvee.jpg')}
-          style={{width: 200, height: 200}}></Image>
-        <FlatList
-          data={demoList}
-          renderItem={({item}) => (
-            <Text>
-              {item.id} {item.name}
-            </Text>
-          )}
-          keyExtractor={item => item.id.toString()}
-          style={{
-            marginTop: 20,
-            backgroundColor: '#1BFC06',
-          }}></FlatList>
-        <CustomComponent />
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <Text>LinkedIn</Text>
+        <Text>GitHub</Text>
+        <Text>hanshpatil1982@gmail.com</Text>
+        <Text></Text>
       </View>
-    </ScrollView>
+      <View style={{padding: 20}}>
+        <FlatList
+          data={projects}
+          renderItem={({item}) => {
+            return (
+              <View style={{marginBottom: 20}}>
+                <View style={{display: 'flex', flexDirection: 'row',justifyContent:'space-between'}}>
+                  <Text> {item.projectName}</Text>
+                  <Text>{item.technologies}</Text>
+                </View>
+
+                <Text>{item.description}</Text>
+              </View>
+            );
+          }}></FlatList>
+      </View>
+    </>
   );
 }
 
